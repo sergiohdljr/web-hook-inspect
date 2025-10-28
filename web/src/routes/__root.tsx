697d9045fd47602508989ffa1162bf599e8b2bb5
sergiohdljr <sergio.tjf1@gmail.com>
2025-10-28 20:41:11 -0300

    feat: add Biome formatter and configure fonts with Geist and JetBrains Mono

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,20 +1,15 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 
 const RootLayout = () => (
-  <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
-    <Outlet />
-    <TanStackRouterDevtools />
-  </>
-)
+	<>
+		<div className="p-2 flex gap-2">
+			<Link to="/" className="[&.active]:font-bold">
+				Home
+			</Link>
+		</div>
+		<hr />
+		<Outlet />
+	</>
+);
 
-export const Route = createRootRoute({ component: RootLayout })
+export const Route = createRootRoute({ component: RootLayout });
