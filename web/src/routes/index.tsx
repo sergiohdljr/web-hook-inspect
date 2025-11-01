2798b3bc2fc367529c0c274291f4c5ce70f48be5
sergiohdljr <sergio.tjf1@gmail.com>
2025-10-31 22:36:18 -0300

    chore: add tailwind utility packages and implement sidebar component

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import { Sidebar } from '../components/sidebar';
 
 export const Route = createFileRoute('/')({
 	component: Index,
@@ -10,7 +11,7 @@ function Index() {
 		<div className="h-screen bg-zinc-900">
 			<PanelGroup direction="horizontal">
 				<Panel defaultSize={20} minSize={15} maxSize={40}>
-					sidebar
+					<Sidebar />
 				</Panel>
 				<PanelResizeHandle className="w-px bg-zinc-700 hover:bg-zinc-600" />
 				<Panel defaultSize={80} minSize={60}>
